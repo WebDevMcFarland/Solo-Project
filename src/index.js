de588418d3c75a40cdb36db1112ead3c5b5b8ca8
WebDevMcFarland <149956718+WebDevMcFarland@users.noreply.github.com>
2024-02-13 15:22:12 -0600

    webpack working

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+function App(){
+    return(
+
+        <h1>Hello World!</h1>
+    )
+}
+
+const root = document.getElementById('root');
+ReactDOM.render(<App/>, root);
